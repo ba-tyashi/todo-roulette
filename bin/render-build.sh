56d39c5898549f6dda544798622cde3e8a8f3409
ba-tyashi <ba-tyashi@example.com>
2026-02-27 15:14:52 +0900

    Update build script to sync data and set exp to 5

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,9 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-#bundle exec rails db:seed
+
+# コメントアウトを外して、カテゴリとキャラのデータを最新にする
+bundle exec rails db:seed
+
+# 本番環境の全キャラクターの必要経験値を 5 に強制上書きする
+bundle exec rails runner "Character.update_all(required_exp: 5)"
